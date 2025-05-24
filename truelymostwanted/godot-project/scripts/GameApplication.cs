c05af46ff24a077c05bcc512ec381d80c521d8ae
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:37:57 +0200

    Added Game Application scene root class

---
+++ truelymostwanted/godot-project/scripts/GameApplication.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+using Godot.Collections;
+using LabyrinthExplorer3D.scripts.core.functions;
+using LabyrinthExplorer3D.scripts.core.menus;
+
+namespace LabyrinthExplorer3D.scripts;
+
+[GlobalClass]
+public partial class GameApplication : Node
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        StopGameFunction.Execute(node: this);
+    }
+}
