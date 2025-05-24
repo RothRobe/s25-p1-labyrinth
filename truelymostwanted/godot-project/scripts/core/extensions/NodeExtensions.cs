776530bc0ed677a1a9e4927f8bca4f67a331ed07
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:24:46 +0200

    create Core/Extensions folder for required type extensions

---
+++ truelymostwanted/godot-project/scripts/core/extensions/NodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace LabyrinthExplorer3D.scripts.core.extensions;
+
+public static class NodeExtensions
+{
+    public static int GetChildrenOfType<T>(this Node node, out List<T> childrenOfType) where T : Node
+    {
+        var children = node.GetChildren();
+        childrenOfType = children.OfType<T>().ToList();
+        return childrenOfType.Count;
+    }
+}
