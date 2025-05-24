95bd69d5ab3986346ced1ae520b2154756cbc607
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:33:20 +0200

    Added specific Player3d Abilities in Game/Abilities

---
+++ truelymostwanted/godot-project/scripts/game/abilties/Player3dPauseAbility.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+using LabyrinthExplorer3D.scripts.core.functions;
+
+namespace LabyrinthExplorer3D.scripts.game.abilties;
+
+[GlobalClass]
+public partial class Player3dPauseAbility : Player3dAbility
+{
+    public override void _OnProcess(double delta)
+    {
+        
+    }
+
+    public override void _OnUnhandledInput(InputEvent @event)
+    {
+        if (!IsAnyInputActionTriggered())
+            return;
+
+        var tree = GetTree();
+        if (tree.IsPaused())
+        {
+            StartGameFunction.Execute(node: this);
+        }
+        else
+        {
+            StopGameFunction.Execute(node: this);
+        }
+    }
+}
