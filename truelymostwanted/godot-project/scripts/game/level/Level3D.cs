51904f35d2aa558a1a2ba92d37ca12e68e90eaaa
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:27:30 +0200

    Created Game/Level Controller and Level as basic classes for level node hierachy

---
+++ truelymostwanted/godot-project/scripts/game/level/Level3D.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace LabyrinthExplorer3D.scripts.game.level;
+
+[GlobalClass]
+public partial class Level3D : Node3D
+{
+    public override void _Ready()
+    {
+        
+    }
+}
---
+++ truelymostwanted/godot-project/scripts/game/level/LevelController3D.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+
+namespace LabyrinthExplorer3D.scripts.game.level;
+
+[GlobalClass]
+public partial class LevelController3D : Node3D
+{
+    [Export] public Level3D CurrentLevel;
+}
