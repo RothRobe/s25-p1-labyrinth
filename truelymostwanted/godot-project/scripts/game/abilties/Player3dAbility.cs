65c2a57edadeae8aed74dcafcf372a3236d70178
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:32:50 +0200

    Created base class in Game/Ability + Game/Components + Game/Player

---
+++ truelymostwanted/godot-project/scripts/game/abilties/Player3dAbility.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+
+namespace LabyrinthExplorer3D.scripts.game.abilties;
+
+[GlobalClass]
+public abstract partial class Player3dAbility : Ability
+{
+    [Export] public Player3D OwningPlayer;
+}
