e830ecb0f634118390fe7ae232e25f77c1a38804
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:34:44 +0200

    Added classes for 3d items, their data, their colliders and collect areas

---
+++ truelymostwanted/godot-project/scripts/game/items/ItemCollectArea3D.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+using LabyrinthExplorer3D.scripts.game.abilties;
+
+namespace LabyrinthExplorer3D.scripts.game.items;
+
+public partial class ItemCollectArea3D : Area3D
+{
+    [Export] public Item3D Item;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        BodyEntered += OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node3D body)
+    {
+        if(body is not Player3D player)
+            return;
+
+        var canGetInventory = player.TryGetAbility<Player3dInventoryAbility>(out var inventoryAbility);
+        if (!canGetInventory)
+            return;
+        
+        inventoryAbility.StoreItem(Item);
+    }
+}
