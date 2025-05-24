c74964bfe17e154ed72bc958935212f6a8f87977
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-05-24 13:37:43 +0200

    Added classes acting as static functions + function buttons to Core/Functions

---
+++ truelymostwanted/godot-project/scripts/core/functions/StartGameFunction.cs
@@ -0,0 +1,22 @@
+﻿using Godot;
+using LabyrinthExplorer3D.scripts.core.menus;
+using LabyrinthExplorer3D.scripts.game.gui;
+
+namespace LabyrinthExplorer3D.scripts.core.functions;
+
+[GlobalClass]
+public partial class StartGameFunction : Function, IFunction
+{
+    public static void Execute(Node node)
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        MenuController.Instance.ToggleCurrentMenu(false);
+        node.GetTree().SetPause(false);   
+        GameUI.Instance.Show();
+    }
+    
+    public override void Execute()
+    {
+        Execute(node: this);   
+    }
+}
